6e253005f5db61f0129c3c4e1c953527078444b6
Martin Chuckeles <martin@jakubik.info>
2014-08-28 15:29:26 +0200

    Client now joining

--- client/js/app.js
+++ client/js/app.js
@@ -2,8 +2,22 @@ var app = angular.module("socketio-chat", [])
 
 	// main chat controller
 	.controller("chatCtrl", function($scope) {
+		// vars
+		$scope.joined = false;
+
 		// join function
 		$scope.join = function() {
+			// check
+			if (!$scope.name)
+				return;
 
+			// connect socket
+			$scope.socket = io("localhost:2500");
+
+			// on connect
+			$scope.socket.on("connect", function() {
+				$scope.socket.emit("join", $scope.name);
+				$scope.joined = true;
+			});
 		};
 	});
