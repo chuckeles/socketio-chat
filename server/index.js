12489201d799fcc1966beaffadd31da69931acbd
Martin Chuckeles <martin@jakubik.info>
2014-08-28 14:36:48 +0200

    Added socket server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var server = express();
 
+var http = require("http").createServer(server);
+var io = require("socket.io")(http);
+
 // routes
 server.get("/", function(req, res) {
 	res.sendfile("client/html/index.html");
@@ -9,6 +12,6 @@ server.use(express.static("client/build"));
 
 // listen
 var port = 2500;
-server.listen(port, function() {
+http.listen(port, function() {
 	console.log("listening on", port);
 });
